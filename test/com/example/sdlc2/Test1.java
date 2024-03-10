f6b92272e0421b5f39619ee44e7bee9887296cb2
Crivera270 <156555255+Crivera270@users.noreply.github.com>
2024-03-10 12:57:10 -0400

    Add files via upload

--- test/com/example/sdlc2/Test1.java
+++ test/com/example/sdlc2/Test1.java
@@ -12,17 +12,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class Test1 {
-    ArrayList<String> booktest = new ArrayList<>();
+    //books can be added to the database
+    //a book can be removed from the database by barcode or title
+    //a book can be checked out.  If so, the due date is no longer "null".
+    //a book can be checked in. If so, the due date is "null".
+    ArrayList<Checkable> booktest = new ArrayList<>();
     //Test class
     //Listit.readItem
     //Remove.RemoveElement
     //Add.addElement
     //SDLC2.main
     @org.junit.jupiter.api.Test
-    @DisplayName("Testing if readItem method in Listit class outputs anything")
-    public void testReadItem(){
-        //ArrayList<String> booktest = new ArrayList<>();
-        booktest.add("Hello, Goodbye, Good morning");
+    @DisplayName("Testing if books can be added to the database")
+    public void testADDItem(){
+        booktest.add(new Book("1","test","test"));
         Listit.readItem(booktest);
 
         Assertions.assertFalse(Listit.readItem(booktest).isEmpty());
@@ -34,11 +37,12 @@ public class Test1 {
     @Test
     @DisplayName("Testing if removeEasy method in Remove class removes")
     public void testremoveElement(){
-        booktest.add("This is the start of the collection");
-        booktest.add("Hello, Goodbye, Good morning");
-        booktest.add("Apple, Banana, Orange");
-        booktest.add("Mylo, Christina, Rafael");
-        booktest.add("A,B,C");
+        booktest.add(new Book("1","test","test"));
+        booktest.add(new Book("1","test","test"));
+        booktest.add(new Book("1","test","test"));
+        booktest.add(new Book("1","test","test"));
+        booktest.add(new Book("1","test","test"));
+
         Assertions.assertEquals(5, Listit.readItem(booktest).size());
 
         Remove.now = 1;
@@ -49,19 +53,39 @@ public class Test1 {
 
     }
 
+
     @Test
-    @DisplayName("Testing")
-    public void testaddElement(){
+    @DisplayName("Testing if a book is able to get checked in")
+    public void testCheckin(){
+        booktest.add(new Book("1","test","test"));
+        booktest.add(new Book("1","test","test"));
+        booktest.get(1).setCheckedIn(false);
+        Listit.readItem(booktest);
 
 
+        Book.pick1=1;
+        Book.checkin = 1;
 
-        Add.addElement(booktest);
+        Book.fortest(booktest);
+
+        Listit.readItem(booktest);
 
     }
     @Test
-    @DisplayName("Testing")
-    public void testMain(){
+    @DisplayName("Testing if a book is able to get checked out")
+    public void testCheckout(){
+        booktest.add(new Book("1","test","test"));
+        booktest.add(new Book("1","test","test"));
+        Listit.readItem(booktest);
 
 
+
+        Book.pick1=2;
+        Book.choices = 1;
+
+        Book.fortest(booktest);
+
+        Listit.readItem(booktest);
     }
+
 }
